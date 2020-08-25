6d8e290a2f32ea0c0655dee9a17439e3149a2887
Devin Heigert <heigertd@gmail.com>
2020-08-24 19:20:36 -0700

    added style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,54 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import logo from '../../images/logo.png'
 import './header.css'
+// TODO: add conditional rendering for menu
 
 export default function Header() {
+    const [dropDownState, setDropDownState] = useState([]);
+
+    useEffect(()=>{
+        setDropDownState('');
+    },[])
+
+    function dropDown(){
+        setDropDownState('true')   
+    }
+    function close(){
+        setDropDownState('')   
+    }
+
     return (
         <div className = 'head'>
             <img className = 'image' src={logo} alt='logo'/>
             <div className = 'nav-bar'>
                 <a href='/' className = 'tab'>Home</a>
                 <a href='/charts' className = 'tab'>Charts</a>
-                <a href='/about-contact' className = 'tab'>About/Contact</a>
+                <a href='/about-contact' className = 'tab'>About</a>
             </div>
+            <button className='dropdown-button' onClick={dropDown}> --- </button>  
         </div>
+        // { dropDownState?
+        //     (<div className = 'head'>
+        //             <div className = 'nav-bar'>
+        //                 <a href='/' className = 'tab'>Home</a>
+        //                 <a href='/charts' className = 'tab'>Charts</a>
+        //                 <a href='/about-contact' className = 'tab'>About</a>
+        //             </div>
+        //             <button onClick={close}>X</button>    
+        //     </div>)
+        //     :
+        //     (<div className = 'head'>
+        //             <img className = 'image' src={logo} alt='logo'/>
+        //             <div className = 'nav-bar'>
+        //                 <a href='/' className = 'tab'>Home</a>
+        //                 <a href='/charts' className = 'tab'>Charts</a>
+        //                 <a href='/about-contact' className = 'tab'>About</a>
+        //             </div>
+        //             <button className='dropdown-button' onClick={dropDown}> --- </button>  
+        //     </div>)
+            
+        // }
     )
 }
+
+
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import slide1 from '../../images/slide1.png'
+import './home.css'
 
 export default function Home() {
     return (
-        <div>
+        <div className='home-page'>
             <Header />
-            <img src={slide1} alt = 'geneology chart' />
-            <h1>World Famous Genealogy Charts</h1>
-            <h3>We make great charts, that make great gifts. For over 50 years we've been providing the world's best genealogy charts. At least that's what we keep hearing from our customers. Thank you for your business over the years.</h3>
+            <div className='home'>
+                <img src={slide1} className='image-home' alt = 'geneology chart' />
+                <h1>World Famous Genealogy Charts</h1>
+                <h3>We make great charts, that make great gifts. For over 50 years we've been providing the world's best genealogy charts. At least that's what we keep hearing from our customers. Thank you for your business over the years.</h3>
+            </div>
         </div>
     )
 }
