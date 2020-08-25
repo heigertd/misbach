c0645a005abf953903d17be6d30d343fe6ed1d89
Devin Heigert <heigertd@gmail.com>
2020-08-25 14:12:07 -0700

    conditional rendering navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import logo from '../../images/logo.png'
 import './header.css'
-// TODO: add conditional rendering for menu
+
 
 export default function Header() {
     const [dropDownState, setDropDownState] = useState([]);
@@ -18,36 +18,29 @@ export default function Header() {
     }
 
     return (
-        <div className = 'head'>
-            <img className = 'image' src={logo} alt='logo'/>
-            <div className = 'nav-bar'>
-                <a href='/' className = 'tab'>Home</a>
-                <a href='/charts' className = 'tab'>Charts</a>
-                <a href='/about-contact' className = 'tab'>About</a>
-            </div>
-            <button className='dropdown-button' onClick={dropDown}> --- </button>  
-        </div>
-        // { dropDownState?
-        //     (<div className = 'head'>
-        //             <div className = 'nav-bar'>
-        //                 <a href='/' className = 'tab'>Home</a>
-        //                 <a href='/charts' className = 'tab'>Charts</a>
-        //                 <a href='/about-contact' className = 'tab'>About</a>
-        //             </div>
-        //             <button onClick={close}>X</button>    
-        //     </div>)
-        //     :
-        //     (<div className = 'head'>
-        //             <img className = 'image' src={logo} alt='logo'/>
-        //             <div className = 'nav-bar'>
-        //                 <a href='/' className = 'tab'>Home</a>
-        //                 <a href='/charts' className = 'tab'>Charts</a>
-        //                 <a href='/about-contact' className = 'tab'>About</a>
-        //             </div>
-        //             <button className='dropdown-button' onClick={dropDown}> --- </button>  
-        //     </div>)
+        <div>
+         { dropDownState?
+            (<div className = 'head'>
+                <div className='dropdown-nav'>      
+                    <a href='/' className = 'tab'>Home</a>
+                    <a href='/charts' className = 'tab'>Charts</a>
+                    <a href='/about-contact' className = 'tab'>About</a>
+                    <button className='dropdown-button-close' onClick={close}>X</button>
+                </div>      
+            </div>)
+            :
+            (<div className = 'head'>
+                    <img className = 'image' src={logo} alt='logo'/>
+                    <div className = 'nav-bar'>
+                        <a href='/' className = 'tab'>Home</a>
+                        <a href='/charts' className = 'tab'>Charts</a>
+                        <a href='/about-contact' className = 'tab'>About</a>
+                    </div>
+                    <button className='dropdown-button' onClick={dropDown}> --- </button>  
+            </div>)
             
-        // }
+        }
+        </div>
     )
 }
 
