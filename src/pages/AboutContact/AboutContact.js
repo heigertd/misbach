9c375e12aab036f15b9c0df43ba42f6269e54118
Devin Heigert <heigertd@gmail.com>
2021-01-21 14:54:03 -0800

    about page redesign

--- src/pages/AboutContact/AboutContact.js
+++ src/pages/AboutContact/AboutContact.js
@@ -6,25 +6,29 @@ import './about-contact.css'
 export default function AboutContact() {
     return (
         <div className="about-page">
-            <div className='about'>
-                <div className='grant'>
-                    <div className='image'>
-                        <img src={Grant} alt='Grant Misbach' />
+            <div className = 'div1'></div>
+            <div className = 'div2'>
+                <h1 className = 'about-title'>About</h1>
+                <div className='about'>
+                    <div className='grant'>
+                        <div className='image'>
+                            <img src={Grant} alt='Grant Misbach' />
+                        </div>
+                        <div className = 'grant-text'>
+                            <p>Grant L. Misbach founded Misbach Enterprises in 1958, and has been supplying the genealogy community with charts, software and other genealogy supplies for over 50 years. Over a million copies of our charts have been sold across the world. Our quality, experience and reputation have allowed us to gain your trust and praise.</p>
+                            <p>Some of our charts focus on function or utility while others focus on presentation. No matter what you are looking for we think you will find it here. All charts come with a 100% satisfaction guarantee. If you don't like the chart just send it back and we will refund your money.</p>
+                            <p>We also house one of the largest collections of charts in PDF format. All of these charts are free for personal use. The collection continues to grow. Feedback and ideas are welcome for future designs.</p>
+                            <p>Our popular GED Browser (software program that publishes your genealogy on the Internet) was heavily used for over 20 years. It's no longer available for download.</p>
+                            <p>You also might enjoy our sister company that develops dynamic custom charts located at TreeSeek.com.</p>
+                            <p>It's our pleasure to provide you world class genealogy charting products. - Thank you and happy researching!</p>
+                        </div>
                     </div>
-                    <div className = 'grant-text'>
-                        <p>Grant L. Misbach founded Misbach Enterprises in 1958, and has been supplying the genealogy community with charts, software and other genealogy supplies for over 50 years. Over a million copies of our charts have been sold across the world. Our quality, experience and reputation have allowed us to gain your trust and praise.</p>
-                        <p>Some of our charts focus on function or utility while others focus on presentation. No matter what you are looking for we think you will find it here. All charts come with a 100% satisfaction guarantee. If you don't like the chart just send it back and we will refund your money.</p>
-                        <p>We also house one of the largest collections of charts in PDF format. All of these charts are free for personal use. The collection continues to grow. Feedback and ideas are welcome for future designs.</p>
-                        <p>Our popular GED Browser (software program that publishes your genealogy on the Internet) was heavily used for over 20 years. It's no longer available for download.</p>
-                        <p>You also might enjoy our sister company that develops dynamic custom charts located at TreeSeek.com.</p>
-                        <p>It's our pleasure to provide you world class genealogy charting products. - Thank you and happy researching!</p>
-                    </div>
-                </div>
-                <h1>Contact Us</h1>
-                <p>Please contact us with any questions or suggestions at info@misbach.org</p>
+                    <h3>Contact Us</h3>
+                    <p>Please contact us with any questions or suggestions at info@misbach.org</p>
 
-                <h1>Refund / Return Policy</h1>
-                <p>If you are dissatisfied for any reason please email us at info@misbach.org requesting a refund. We will refund your order, and give you instructions to return the product. Thanks!</p>
+                    <h3>Refund / Return Policy</h3>
+                    <p>If you are dissatisfied for any reason please email us at info@misbach.org requesting a refund. We will refund your order, and give you instructions to return the product. Thanks!</p>
+                </div>
             </div>
         </div>
     )
