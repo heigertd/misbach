0f011128fe0dc69e651364bd72b629de9b4d56fc
Devin Heigert <heigertd@gmail.com>
2021-01-22 16:20:49 -0800

    chart info updated

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -11,8 +11,10 @@ export default function Card(props) {
                 <h3 className='card-text-title'>{props.text}</h3>
                 <img className='card-image' src={props.image} alt= 'nine generation fan frame chart'/>
             </div>
-            <div className = 'card-content'> 
+            <div className = 'card-content'>
+                <p>Size: {props.size}</p><br/>
                 <p className='card-text'>{props.description}</p>
+                <h4>{props.price}</h4>
             </div>
         </div>
     )
