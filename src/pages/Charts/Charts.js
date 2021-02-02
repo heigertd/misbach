d946696fc4f960bdf344a4effeaa43674fb256cc
Devin Heigert <heigertd@gmail.com>
2021-02-01 17:28:58 -0800

    removed line

--- src/pages/Charts/Charts.js
+++ src/pages/Charts/Charts.js
@@ -160,7 +160,6 @@ export default function Charts() {
             download: familygrouppdf
         },
     ])
-    const [dropdownState, setDropdownState] = useState([]);
 
     useEffect(()=>{
         setFilteredItems(itemState);
